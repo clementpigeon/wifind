ee9c2cebab4480908ddd98a448c6e83140b1b3e9
Oualid <jouhri.oualid@gmail.com>
2013-12-07 15:29:52 +0100

    bundle update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Wifind::Application.routes.draw do
   get "spot/index"
+
+  root :to => "spot#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
