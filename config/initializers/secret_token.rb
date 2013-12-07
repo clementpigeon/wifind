b45bb8cca50b08bdde223bc0d63baa5c2870b1e4
Oualid <jouhri.oualid@gmail.com>
2013-12-07 14:42:24 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wifind::Application.config.secret_key_base = '278fc9b5f42db93c27785abd47abb58bd6aa2270245ff002491b5053e1518117760a07b780011e5a8d0d6b6b4db2a5b030af15ecff2bc74dd159746c52273f0a'
